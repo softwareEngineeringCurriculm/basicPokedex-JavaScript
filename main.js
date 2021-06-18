7c14987ddd8ce71bf439d1149f2bff73966e57cc
alexAtGalvanize <alex.jean-baptiste@galvanize.com>
2021-06-17 21:32:55 -0400

    Edited exercise to potentially increase clarity for learners

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 //PART I D.O.M. Manipulation
 
 // Select the search button pokeball by its HTML Id Attribute
-var searchBtn = document.getElementById('search-btn'); // search button pokeball
+var searchBtn = document.getElementById('ADD CODE'); // search button pokeball
 
 // Select the search field input by its HTML Id Attribute
-var inputField = document.getElementById('name-input'); // search field input
+var inputField = document.getElementById('ADD CODE'); // search field input
 
 // Select the pokemon name screen by it's HTML Id Attribute
-var nameScreen = document.getElementById('name-screen'); //pokemon name-screen
+var nameScreen = document.getElementById('ADD CODE'); //pokemon name-screen
 
 // Select the pokemon image screen by it's HTML Id Attribute
-var imageScreen = document.getElementById('main-screen'); // pokemon image screen
+var imageScreen = document.getElementById('ADD CODE'); // pokemon image screen
 
 // Select the height and weight screen by it's HTML Id Attribute
-var aboutScreen = document.getElementById('about-screen'); // about-text screen
+var aboutScreen = document.getElementById('ADD CODE'); // about-text screen
 
 // Select the type screen by it's HTML Id Attribute
-var typeScreen = document.getElementById('type-screen'); // pokemon type screen
+var typeScreen = document.getElementById('ADD CODE'); // pokemon type screen
 
 // Select the pokemon id number screen by it's HTML Id Attribute
-var idScreen = document.getElementById('id-screen'); // pokemon id number screen
+var idScreen = document.getElementById('ADD CODE'); // pokemon id number screen
 
 //PART II Fetching the Pokemon Data from the PokeAPI
 
@@ -38,7 +38,7 @@ var getPokemonData = (pokemon) => {
       //create a format appropriate pokemon id in order to plug into the pokemon.com image url
       var id = ('00' + data.id).slice(-3);
       //change the background image of the image screen element to the appropriate pokemon image
-      imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
+      imageScreen.style.backgroundImage = `url('ADD CODE')`;
       //change the text of the name screen element to the appropriate pokemon name
       nameScreen.innerHTML = data.name;
       //change the text of the type screen element to the appropriate pokemon type
@@ -59,7 +59,7 @@ var getPokemonData = (pokemon) => {
 //Create an event listner that attaches to the search button poke ball and activates when a user 'clicks' on it!
 //The event listener should call our pokemon data function from PART II
 
-searchBtn.addEventListener('click', () => getPokemonData(inputField.value));
+searchBtn.addEventListener('click', () => getPokemonData("ADD CODE"));
 
 
 // <!-- Copyright (c) 2021 by oryam (https://codepen.io/oryamne/pen/vYKXbgZ)
